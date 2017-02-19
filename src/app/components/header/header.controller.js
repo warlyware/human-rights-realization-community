5b6b3249a3c3465327eafd47accb28fae0d83be2
warlyware <danward@gmail.com>
2017-02-19 13:58:04 -0500

    Import fonts, apply fonts to home, header, about

--- src/app/components/header/header.controller.js
+++ src/app/components/header/header.controller.js
@@ -5,14 +5,16 @@
         .module('hrrc-app.components')
         .controller('HeaderController', HeaderController);
 
-        HeaderController.$inject = ['$mdSidenav'];
+        HeaderController.$inject = ['$mdSidenav', '$mdMedia'];
 
-        function HeaderController($mdSidenav) {
+        function HeaderController($mdSidenav, $mdMedia) {
             var vm = this;
 
             vm.toggleSideNav = function() {
                 $mdSidenav('side-nav').toggle();
             }
+
+            vm.$mdMedia = $mdMedia;
 
             return vm;
         }
