d402c3a1c60f7bb0ecf024c8e61484457c27cbf9
warlyware <danward@gmail.com>
2017-02-19 15:28:00 -0500

    Add dummy data for 6 articles on teaching resources page

--- src/app/teaching-resources/teaching-resources.controller.js
+++ src/app/teaching-resources/teaching-resources.controller.js
@@ -12,6 +12,45 @@ angular
         vm.$mdMedia = $mdMedia;
         vm.bodyCopy = bodyCopy.teachingResources;
 
+        vm.articleData = [
+            {
+                title: 'Article 1',
+                author: 'Mr. Man',
+                blurb: 'A short description that tells you everything you want to know.',
+                body: "You can work and carry-on and put lots of little happy things in here. You are only limited by your imagination. You don't have to spend all your time thinking about what you're doing, you just let it happen. Isn't that fantastic? You can just push a little tree out of your brush like that. In nature, dead trees are just as normal as live trees. Be so very light. Be a gentle whisper. There is no right or wrong - as long as it makes you happy and doesn't hurt anyone. At home you have unlimited time. Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect. We'll make some happy little bushes here. Now it's beginning to make a little sense. We want to use a lot pressure while using no pressure at all. Happy painting, God bless. A big strong tree needs big strong roots. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. Just make little strokes like that. I thought today we would do a happy little picture. Put your feelings into it, your heart, it's your world. Now then, let's play."
+            },
+            {
+                title: 'Article 2',
+                author: 'Ms. Woman',
+                blurb: 'A short description that tells you everything you want to know.',
+                body: "You can work and carry-on and put lots of little happy things in here. You are only limited by your imagination. You don't have to spend all your time thinking about what you're doing, you just let it happen. Isn't that fantastic? You can just push a little tree out of your brush like that. In nature, dead trees are just as normal as live trees. Be so very light. Be a gentle whisper. There is no right or wrong - as long as it makes you happy and doesn't hurt anyone. At home you have unlimited time. Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect. We'll make some happy little bushes here. Now it's beginning to make a little sense. We want to use a lot pressure while using no pressure at all. Happy painting, God bless. A big strong tree needs big strong roots. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. Just make little strokes like that. I thought today we would do a happy little picture. Put your feelings into it, your heart, it's your world. Now then, let's play."
+            },
+            {
+                title: 'Article 3',
+                author: 'Mr. PacMan',
+                blurb: 'A short description that tells you everything you want to know.',
+                body: "You can work and carry-on and put lots of little happy things in here. You are only limited by your imagination. You don't have to spend all your time thinking about what you're doing, you just let it happen. Isn't that fantastic? You can just push a little tree out of your brush like that. In nature, dead trees are just as normal as live trees. Be so very light. Be a gentle whisper. There is no right or wrong - as long as it makes you happy and doesn't hurt anyone. At home you have unlimited time. Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect. We'll make some happy little bushes here. Now it's beginning to make a little sense. We want to use a lot pressure while using no pressure at all. Happy painting, God bless. A big strong tree needs big strong roots. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. Just make little strokes like that. I thought today we would do a happy little picture. Put your feelings into it, your heart, it's your world. Now then, let's play."
+            },
+            {
+                title: 'Article 4',
+                author: 'Ms. PacMan',
+                blurb: 'A short description that tells you everything you want to know.',
+                body: "You can work and carry-on and put lots of little happy things in here. You are only limited by your imagination. You don't have to spend all your time thinking about what you're doing, you just let it happen. Isn't that fantastic? You can just push a little tree out of your brush like that. In nature, dead trees are just as normal as live trees. Be so very light. Be a gentle whisper. There is no right or wrong - as long as it makes you happy and doesn't hurt anyone. At home you have unlimited time. Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect. We'll make some happy little bushes here. Now it's beginning to make a little sense. We want to use a lot pressure while using no pressure at all. Happy painting, God bless. A big strong tree needs big strong roots. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. Just make little strokes like that. I thought today we would do a happy little picture. Put your feelings into it, your heart, it's your world. Now then, let's play."
+            },
+            {
+                title: 'Article 5',
+                author: 'Bob Bobberson',
+                blurb: 'A short description that tells you everything you want to know.',
+                body: "You can work and carry-on and put lots of little happy things in here. You are only limited by your imagination. You don't have to spend all your time thinking about what you're doing, you just let it happen. Isn't that fantastic? You can just push a little tree out of your brush like that. In nature, dead trees are just as normal as live trees. Be so very light. Be a gentle whisper. There is no right or wrong - as long as it makes you happy and doesn't hurt anyone. At home you have unlimited time. Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect. We'll make some happy little bushes here. Now it's beginning to make a little sense. We want to use a lot pressure while using no pressure at all. Happy painting, God bless. A big strong tree needs big strong roots. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. Just make little strokes like that. I thought today we would do a happy little picture. Put your feelings into it, your heart, it's your world. Now then, let's play."
+            },
+            {
+                title: 'Article 6',
+                author: 'El Ron Hubarino',
+                blurb: 'A short description that tells you everything you want to know.',
+                body: "You can work and carry-on and put lots of little happy things in here. You are only limited by your imagination. You don't have to spend all your time thinking about what you're doing, you just let it happen. Isn't that fantastic? You can just push a little tree out of your brush like that. In nature, dead trees are just as normal as live trees. Be so very light. Be a gentle whisper. There is no right or wrong - as long as it makes you happy and doesn't hurt anyone. At home you have unlimited time. Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect. We'll make some happy little bushes here. Now it's beginning to make a little sense. We want to use a lot pressure while using no pressure at all. Happy painting, God bless. A big strong tree needs big strong roots. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. Just make little strokes like that. I thought today we would do a happy little picture. Put your feelings into it, your heart, it's your world. Now then, let's play."
+            }
+        ]
+
         return vm;
     };
 
