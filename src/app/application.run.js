d8d29e35d54b8eb7a8c802a573d09b240e652e34
warlyware <danward@gmail.com>
2017-02-26 19:11:02 -0500

    Add initAppData on firebase service, use in application run block

--- src/app/application.run.js
+++ src/app/application.run.js
@@ -4,12 +4,15 @@ angular
     .module('hrrc-app')
     .run(run);
 
-    run.$injector = ['$window', '$rootScope'];
+    run.$injector = ['$window', '$rootScope', 'FirebaseService'];
 
-    function run($window, $rootScope) {
+    function run($window, $rootScope, FirebaseService) {
         $rootScope.$on('$stateChangeSuccess', function() {
             $window.scrollTo(0, 0);
         });
+
+        FirebaseService.initAppData($rootScope);
+
     }
 
 })();
--- src/app/services/firebase.service.js
+++ src/app/services/firebase.service.js
@@ -10,7 +10,14 @@
         function FirebaseService($window, $firebaseObject) {
 
             return {
+                initAppData: initAppData,
                 getSyncedScopeObject: getSyncedScopeObject
+            }
+
+            function initAppData(scope) {
+                var firebaseReference = $window.firebase.database().ref();
+                var syncObject = $firebaseObject(firebaseReference);
+                return syncObject.$bindTo(scope, 'appData');
             }
 
             function getSyncedScopeObject(scope, firebaseReferenceString) {
