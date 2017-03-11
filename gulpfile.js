9055bd339accd79ea47fe79be19ffd4bcd6a9170
warlyware <danward@gmail.com>
2017-03-11 11:56:13 -0500

    Add images to build process, add home and library hero images

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,30 @@ var concatCss = require('gulp-concat-css');
 
 gulp.task('styles', function() {
     return gulp.src(config.paths.src.styles)
-        .pipe(sass.sync({includePaths: config.paths.src.styles_imports}).on('error', sass.logError))
+        .pipe(sass.sync({ includePaths: config.paths.src.styles_imports }).on('error', sass.logError))
         .pipe(concatCss('style.css'))
         .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy:html', function() {
-  return gulp.src(config.paths.src.html)
-    .pipe(gulp.dest('build'));
+    return gulp.src(config.paths.src.html)
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy:js', function() {
-  return gulp.src(config.paths.src.js)
-    .pipe(gulp.dest('build'));
+    return gulp.src(config.paths.src.js)
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy:node_modules', function() {
-  return gulp.src(config.paths.node_modules)
-    .pipe(gulp.dest('build/node_modules'));
+    return gulp.src(config.paths.node_modules)
+        .pipe(gulp.dest('build/node_modules'));
 });
+
+gulp.task('copy:images', function() {
+    return gulp.src(config.paths.images)
+        .pipe(gulp.dest('build/img'));
+})
 
 gulp.task('watch', function() {
     gulp.watch(config.paths.src.html, ['copy:html']);
@@ -33,4 +38,4 @@ gulp.task('watch', function() {
     gulp.watch(config.paths.src.styles, ['styles']);
 });
 
-gulp.task('default', ['styles', 'copy:html', 'copy:js', 'copy:node_modules', 'watch']);
+gulp.task('default', ['styles', 'copy:html', 'copy:js', 'copy:node_modules', 'copy:images', 'watch']);
