303a352f76a8a82fb6ec9c3df61ac2e312dceff5
warlyware <danward@gmail.com>
2017-03-25 11:40:29 -0400

    Improve uniformity of spacing

--- src/app/body-copy/body-copy.constant.js
+++ src/app/body-copy/body-copy.constant.js
@@ -8,7 +8,7 @@
                 'main': 'The focus of the Human Rights Realization Community is to build an expansive collection of academic human rights related resources and support educational institutions in implementing human rights into their curriculum. As a group of human rights proponents, we have had the opportunity to inspire many students to become more involved in the human rights movement. We will be accept content submissions and utilize them to build the quality of human rights education our partner institutions utilize in their courses. If you have any questions or would like to make a submission, please email',
             },
             'home': {
-                'missionStatement': 'At HRRC, we envision a world where human rights are the cornerstone of a student’s education.'
+                'missionStatement': 'At the Human Rights Realization Community we envision a world where human rights are the cornerstone of a student’s education.'
             },
             'submissions': {
                 'main': 'In the hopes of creating a truly collaborative space for sharing knowledge about human rights, we will be posting blogs, articles, and papers written by the Human Rights Realization Community.  This section is dedicated to the community.  Feel free to browse through the contributions and share them with your friends and family.  Everyone is free to make submissions.  To contribute to the growing collection send your submissions to:'
